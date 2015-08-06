321b7cfc2294c4e61bee9a50ec7ae70becf9ea42
Venus Ang <venusang24@gmail.com>
2015-08-05 20:13:55 -0700

    Adding sort for project tasks - All, Active, Completed.  Cleaned-up UI to accommodate new sorting feature for tasks. Added badge to display total number of tasks for all types.  Inserted dummy placeholder data for total number of active and completed tasks in their respective sort tabs.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
 
+    
+
     respond_to do |format|
       format.html
       format.json { render :json => @projects }
